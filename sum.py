c66c2b846cfa5e0727fe58eda0839bbfaaa8153c
EstherAmongin <estheramongin@gmail.com>
2018-11-28 12:09:05 +0300

    edited code

--- sum.py
+++ sum.py
@@ -1,15 +1,14 @@
-def compute_sum(list1):
-    if not isinstance(list1, list):
-        return 'Invalid argument type. This should be a list'
+def sum(list1):
+    
     mysum=0
     for i in list1:
         if isinstance(i,int):
             mysum+=i
 
         elif isinstance(i,list):
-            mysum+=compute_sum(i)
+            mysum+=sum(i)
         
     return mysum
 
 if __name__ == '__main__':
-    print(compute_sum(['x',1,[2,3]]))
+    print(sum([2,1,[2,3]]))
